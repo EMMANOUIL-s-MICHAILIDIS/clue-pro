5b1c14bcf42d6bd8e29ef6cfdf415e5c7383c63f
manos <emmanuel.s.michailidis@outlook.com>
2025-03-26 12:57:17 +0000

    d2

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 #include "ClueClusterer.h"
 #include <iostream>
 
-int main() {
-    double dc = 0.05;              // Απόσταση cutoff για υπολογισμό πυκνότητας
-    int numClusters = 3;         // Πλήθος αναμενόμενων clusters
+int main()
+{
+    double dc = 0.05;    // Απόσταση cutoff για υπολογισμό πυκνότητας
+    int numClusters = 3; // Πλήθος αναμενόμενων clusters
     std::string filepath = "data/particles.csv";
 
     ClueClusterer clue(dc);
 
-    if (!clue.loadCSV(filepath)) {
+    if (!clue.loadCSV(filepath))
+    {
         std::cerr << "Error loading CSV file." << std::endl;
         return 1;
     }
